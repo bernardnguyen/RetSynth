ab57697c78c88cdb70d40f9c80ef14b89f039b69
lwhitmore <leanne382@gmail.com>
2020-09-07 20:24:44 -0700

    update rs_gc.py so that it can has all the necessary parameters for generating html

--- rsgc/rs_gc.py
+++ rsgc/rs_gc.py
@@ -780,7 +780,7 @@ def main():
 
             if args.output_html:
                 print("STATUS: writing html file")
-                gh.HtmlOutput(len(targets), args.output_path, args.flux_balance_analysis, args.figures_graphviz, args.output_path+"/Results.html")
+                gh.HtmlOutput(len(targets), args.output_path, args.flux_balance_analysis, args.figures_graphviz, database, args.output_path+"/Results.html")
 
         '''Remove all temporary images'''
         shutil.rmtree(temp_imgs_PATH)
