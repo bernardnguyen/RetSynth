05ab4a0923040c94f3243076a68b2ac588994d36
lwhitmore <leanne382@gmail.com>
2020-10-11 07:41:50 -0700

    update rsgc/Database/database_functions.py so that code generates file of kegg ids and inchistrings and doesn't have to look them up every time a database is made

--- rsgc/Database/database_functions.py
+++ rsgc/Database/database_functions.py
@@ -5,6 +5,7 @@ __description__ = 'Code contains database functions'
 
 import pubchempy
 import re
+import os
 from copy import deepcopy
 try:
     import urllib.request as urllib2
@@ -12,7 +13,19 @@ try:
 except:
     import urllib2
     import httplib
+PATH = os.path.dirname(os.path.abspath(__file__))
+print (PATH)
 KEGG = 'http://rest.kegg.jp/'
+translate_dict = {}
+if os.path.exists(PATH+"/KEGG2INCHI.txt") is True:
+    
+    with open(PATH+"/KEGG2INCHI.txt", "r") as fin:
+        for line in fin:
+            line = line.strip()
+            larray = line.split('\t')
+            translate_dict[larray[0]] = str(larray[1])+"\t"+str(larray[2])
+translatefile = open(PATH+"/KEGG2INCHI.txt", "a")
+    
 
 def verbose_print(verbose, line):
     '''verbose print function'''
@@ -32,33 +45,41 @@ def extract_KEGG_data(url, verbose):
 
 def kegg2pubcheminchi(cpd, verbose):
     '''Convvert kegg ID to InChI value'''
-    darray = extract_KEGG_data(KEGG+'get/'+cpd, verbose)
+    if cpd in translate_dict:
+        larray = translate_dict[cpd].split("\t")
+        for i, x in enumerate(larray):
+            larray[i] = None if larray[i] == 'None' else larray[i]
+        return(larray[0], larray[1])
+    
+    else:
+        darray = extract_KEGG_data(KEGG+'get/'+cpd, verbose)
 
-    inchicpd = None
-    cas = None
-    if darray:
-        for value in darray:
-            array = value.split()
-            if 'PubChem:' in array:
-                index = array.index('PubChem:')
-                sid = array[index+1]
-                try:
-                    substance = pubchempy.Substance.from_sid(sid)
-                    substance_cids = substance.cids
-                    if substance_cids:
-                        try:
-                            compounds = pubchempy.get_compounds(substance_cids[0])
-                            if compounds:
-                                inchicpd = compounds[0].inchi
-                        except (pubchempy.PubChemHTTPError, httplib.BadStatusLine, urllib2.URLError):
-                            pass
-                except (pubchempy.PubChemHTTPError, httplib.BadStatusLine, urllib2.URLError):
-                    verbose_print(verbose, 'WARNING: Could not get substance for {} {}'.format(sid, cpd))
-                    pass
-            if 'CAS:' in array:
-                index = array.index('CAS:')
-                cas = array[index+1]
-    return (inchicpd, cas)
+        inchicpd = None
+        cas = None
+        if darray:
+            for value in darray:
+                array = value.split()
+                if 'PubChem:' in array:
+                    index = array.index('PubChem:')
+                    sid = array[index+1]
+                    try:
+                        substance = pubchempy.Substance.from_sid(sid)
+                        substance_cids = substance.cids
+                        if substance_cids:
+                            try:
+                                compounds = pubchempy.get_compounds(substance_cids[0])
+                                if compounds:
+                                    inchicpd = compounds[0].inchi
+                            except (pubchempy.PubChemHTTPError, httplib.BadStatusLine, urllib2.URLError):
+                                pass
+                    except (pubchempy.PubChemHTTPError, httplib.BadStatusLine, urllib2.URLError):
+                        verbose_print(verbose, 'WARNING: Could not get substance for {} {}'.format(sid, cpd))
+                        pass
+                if 'CAS:' in array:
+                    index = array.index('CAS:')
+                    cas = array[index+1]
+        translatefile.write(cpd+"\t"+str(inchicpd)+"\t"+str(cas)+"\n")
+        return (inchicpd, cas)
 
 def get_KEGG_IDs(ID, compartment, KEGGdict):
     '''Retrieve KEGG IDs'''
