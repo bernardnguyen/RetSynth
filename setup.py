98f96450928212eee10f91362e581e191d46b67f
lwhitmore <leanne382@gmail.com>
2020-12-12 16:49:44 -0800

    fix bug in setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(name="RetSynth_gc",
           'tqdm==4.47.0',
           'scipy==1.5.1',
           "pillow",
-          "lxml", "bio"
+          "lxml", "bio",
           "requests"
       ],
       include_package_data=True,
